81fc895058eca4344bc251d84d5057154d7505b5
Dmitry Dudunov <dydynovdima6@gmail.com>
2022-01-04 22:27:49 +0300

    fix: route button logic

--- lib/presenter/main_screen/widgets/route_button.dart
+++ lib/presenter/main_screen/widgets/route_button.dart
@@ -1,5 +1,3 @@
-import 'package:edu_homework/utils/routes.dart';
-import 'package:edu_homework/utils/widgets/error_page.dart';
 import 'package:flutter/material.dart';
 
 import 'custom_elevated_btn.dart';
@@ -8,36 +6,22 @@ class RouteButton extends StatelessWidget {
   const RouteButton({
     Key? key,
     required this.routeName,
+    required this.onPush,
   }) : super(key: key);
 
   final String routeName;
+  final VoidCallback onPush;
 
   @override
   Widget build(BuildContext context) {
     return CustomElevatedBtn(
-      onPush: () => _navigateToPage,
+      onPush: onPush,
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           Text(routeName),
           const Icon(Icons.arrow_forward),
         ],
-      ),
-    );
-  }
-
-  void _navigateToPage(context) {
-    var pageRoutes = Routes.pageRoutes;
-
-    Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (_) {
-          if (pageRoutes.containsKey(routeName)) {
-            return pageRoutes[routeName]!;
-          }
-          return StubPage(title: routeName);
-        },
       ),
     );
   }
